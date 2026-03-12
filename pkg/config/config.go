b080ba89592751dbd1e45b547b07ce22d9efcc14
krishna thakur <115836360+krsnaimp@users.noreply.github.com>
2026-03-12 22:02:19 +0530

    feat(config): add Slack webhook support via environment variable

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -3,8 +3,9 @@ package config
 import "github.com/spf13/viper"
 
 type Config struct {
-	PostgresURL string
-	BackupDir   string
+	PostgresURL  string
+	BackupDir    string
+	SlackWebhook string
 }
 
 func LoadConfig() (*Config, error) {
@@ -14,8 +15,9 @@ func LoadConfig() (*Config, error) {
 	viper.AutomaticEnv()
 
 	cfg := &Config{
-		PostgresURL: viper.GetString("POSTGRES_URL"),
-		BackupDir:   viper.GetString("backup_dir"),
+		PostgresURL:  viper.GetString("POSTGRES_URL"),
+		BackupDir:    viper.GetString("backup_dir"),
+		SlackWebhook: viper.GetString("SLACK_WEBHOOK_URL"),
 	}
 
 	return cfg, nil
