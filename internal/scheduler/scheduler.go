59c9d1cbd3f01e9c769666d6de89f752adbbbfe3
krishna thakur <115836360+krsnaimp@users.noreply.github.com>
2026-03-13 20:16:47 +0530

    feat(scheduler): implement cron based internal scheduler

--- internal/scheduler/scheduler.go
+++ internal/scheduler/scheduler.go
@@ -1,1 +1,40 @@
 package scheduler
+
+import (
+	"log"
+
+	"github.com/robfig/cron/v3"
+)
+
+type Scheduler struct {
+	cron *cron.Cron
+}
+
+func New() *Scheduler {
+
+	c := cron.New()
+
+	return &Scheduler{
+		cron: c,
+	}
+}
+
+func (s *Scheduler) AddJob(schedule string, job func()) error {
+
+	_, err := s.cron.AddFunc(schedule, job)
+
+	return err
+}
+
+func (s *Scheduler) Start() {
+
+	log.Println("Schedular Started...")
+
+	s.cron.Start()
+}
+
+func (s *Scheduler) Stop() {
+
+	s.cron.Stop()
+
+}
