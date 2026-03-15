096f8c5e9d063fd18403433c24705bf120576c25
krishna thakur <115836360+krsnaimp@users.noreply.github.com>
2026-03-15 15:56:36 +0530

    refactor(cli): replace local backup execution with grpc client call

--- cmd/backup-cli/cmd/backup.go
+++ cmd/backup-cli/cmd/backup.go
@@ -3,15 +3,12 @@ package cmd
 import (
 	"context"
 	"fmt"
-	"os"
-	"os/signal"
-	"syscall"
+	"log"
+	"time"
 
-	"github.com/iShinzoo/BackUpData/internal/core"
-	"github.com/iShinzoo/BackUpData/internal/core/worker"
-	"github.com/iShinzoo/BackUpData/internal/db/postgres"
-	"github.com/iShinzoo/BackUpData/internal/notification/slack"
+	pb "github.com/iShinzoo/BackUpData/proto"
 	"github.com/spf13/cobra"
+	"google.golang.org/grpc"
 )
 
 var backupCmd = &cobra.Command{
@@ -19,87 +16,28 @@ var backupCmd = &cobra.Command{
 	Short: "Execute database backup",
 	Run: func(cmd *cobra.Command, args []string) {
 
-		ctx, cancel := signal.NotifyContext(
-			context.Background(),
-			os.Interrupt,
-			syscall.SIGTERM,
-		)
+		conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer conn.Close()
+
+		client := pb.NewBackupServiceClient(conn)
+
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 
 		defer cancel()
 
-		// ---- S3 TEST START ----
+		resp, err := client.RunBackup(ctx, &pb.BackupRequest{
+			Database: "postgres-db",
+		})
 
-		// ctxTest := context.Background()
-
-		// s3Store, err := s3.New("backup-cli-test-bucket")
-		// if err != nil {
-		// 	panic(err)
-		// }
-
-		// fmt.Println("S3 client initialized successfully")
-
-		// file, err := os.Open("test.sql")
-		// if err != nil {
-		// 	panic(err)
-		// }
-
-		// err = s3Store.Save(ctxTest, "test.sql", file)
-		// if err != nil {
-		// 	panic(err)
-		// }
-
-		// fmt.Println("File uploaded successfully to S3")
-
-		// ---- S3 TEST END ----
-
-		jobs := make(chan core.BackupJob, 10)
-		results := make(chan core.BackupResult)
-
-		pool := worker.WorkerPool{
-			Workers: 3,
+		if err != nil {
+			log.Fatal(err)
 		}
 
-		pgExecutor := postgres.Executor{}
-
-		var notifier core.Notifier
-
-		webhook := os.Getenv("SLACK_WEBHOOK_URL")
-		if webhook != "" {
-			notifier = slack.New(webhook)
-		}
-
-		handler := func(ctx context.Context, job core.BackupJob) core.BackupResult {
-			return core.BackupHandler(ctx, job, &pgExecutor, notifier)
-		}
-
-		go pool.Run(ctx, jobs, results, handler)
-
-		jobs <- core.BackupJob{
-			Name: "db1",
-			URL:  "postgres://backup:backup@localhost:5432/testdb",
-		}
-
-		jobs <- core.BackupJob{
-			Name: "db2",
-			URL:  "postgres://backup:backup@localhost:5432/testdb",
-		}
-
-		jobs <- core.BackupJob{
-			Name: "db3",
-			URL:  "postgres://backup:backup@localhost:5432/testdb",
-		}
-
-		close(jobs)
-
-		for r := range results {
-
-			if r.Error != nil {
-				fmt.Println("Backup FAILED:", r.Name, r.Error)
-				continue
-			}
-
-			fmt.Println("Backup finished:", r.Name)
-		}
+		fmt.Println("Daemon Response:", resp.Status)
 	},
 }
 
