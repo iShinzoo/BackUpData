40628a8cc1543d0dcea7d9a3396bf1837ba11703
krishna thakur <115836360+krsnaimp@users.noreply.github.com>
2026-03-29 23:38:10 +0530

    fix(s3): add minio support and correct retry logic with buffered reader

--- internal/storage/s3/s3.go
+++ internal/storage/s3/s3.go
@@ -1,27 +1,45 @@
 package s3
 
 import (
+	"bytes"
 	"context"
 	"io"
 	"time"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	awsS3 "github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
 type S3Storage struct {
-	client *s3.Client
+	client *awsS3.Client
 	bucket string
 }
 
 func New(bucket string) (*S3Storage, error) {
 
-	cfg, err := config.LoadDefaultConfig(context.Background())
+	cfg, err := config.LoadDefaultConfig(context.Background(),
+		config.WithRegion("us-east-1"),
+		config.WithEndpointResolverWithOptions(
+			aws.EndpointResolverWithOptionsFunc(
+				func(service, region string, options ...interface{}) (aws.Endpoint, error) {
+					return aws.Endpoint{
+						URL:           "http://localhost:9000",
+						SigningRegion: "us-east-1",
+					}, nil
+				},
+			),
+		),
+	)
+
 	if err != nil {
 		return nil, err
 	}
 
-	client := s3.NewFromConfig(cfg)
+	client := awsS3.NewFromConfig(cfg, func(o *awsS3.Options) {
+		o.UsePathStyle = true
+	})
 
 	return &S3Storage{
 		client: client,
@@ -35,10 +53,13 @@ func (s *S3Storage) Save(
 	r io.Reader,
 ) error {
 
-	var err error
-
 	ctx, cancel := context.WithTimeout(ctx, 5*time.Minute)
 	defer cancel()
+
+	data, err := io.ReadAll(r)
+	if err != nil {
+		return err
+	}
 
 	for i := 0; i < 3; i++ {
 
@@ -47,13 +68,15 @@ func (s *S3Storage) Save(
 			&s3.PutObjectInput{
 				Bucket: &s.bucket,
 				Key:    &name,
-				Body:   r,
+				Body:   bytes.NewReader(data),
 			},
 		)
 
 		if err != nil {
 			return nil
 		}
+
+		time.Sleep(time.Duration(1<<i) * time.Second)
 	}
 
 	return err
